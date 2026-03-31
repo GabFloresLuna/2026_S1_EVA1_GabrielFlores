1af4ba47f5dcdb67f26e801368ff5a4344261a52
GabFloresLuna <gab.floresl@duocuc.cl>
2026-03-31 11:09:18 -0300

    Correción Solicitud

--- src/main/java/cl/duoc/gabrielflores/model/Solicitud.java
+++ src/main/java/cl/duoc/gabrielflores/model/Solicitud.java
@@ -3,11 +3,11 @@ package cl.duoc.gabrielflores.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,7 +25,7 @@ public class Solicitud
     private String especialidadString;
     @NotNull
     private String estadoString;
-    @FutureOrPresent
+    @PastOrPresent
     private LocalDateTime fechaRegistro;
     @Min(value= 1, message= "El nivel debe ser mínimo 1")
     @Max(value= 5, message= "El nivel debe ser máximo 5")
