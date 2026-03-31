df5e3c4049bb40d46fa194c1c0f92ad6eb408996
GabFloresLuna <gab.floresl@duocuc.cl>
2026-03-31 10:08:40 -0300

    Springboot inicial

---
+++ src/main/java/cl/duoc/gabrielflores/GabrielfloresApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.gabrielflores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabrielfloresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrielfloresApplication.class, args);
+	}
+
+}
