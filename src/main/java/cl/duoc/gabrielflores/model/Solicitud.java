6c3f0732bf1cedc8ed3759ad842628f20b4b28a7
GabFloresLuna <gab.floresl@duocuc.cl>
2026-04-02 11:47:51 -0300

    Se agrega atributo rut con respectivas validaciones

--- src/main/java/cl/duoc/gabrielflores/model/Solicitud.java
+++ src/main/java/cl/duoc/gabrielflores/model/Solicitud.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,9 @@ public class Solicitud
     @NotNull(message= "El id no puede ser nulo")
     @Min(value= 1, message= "El id debe ser mínimo 1")
     private Long id;
+    @Pattern(regexp = "^(\\d{1,2}(?:\\.?\\d{3}){2}-?[\\dkK])$", message = "Formato de RUT erróneo")
+    @NotBlank(message = "El rut no puede ser blanco")
+    private String rut;
     @NotBlank(message= "el nombre no puede ser blanco")
     private String nombreString;
     @NotBlank(message= "la especialidad no puede ser blanco")
